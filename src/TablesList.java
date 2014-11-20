a935115fbeaca64743c89d84da1b63c8e24f922f
fcm2009 <fcm2009@gmail.com>
2014-11-20 12:45:03 +0300

    Changes to be committed:

--- src/TablesList.java
+++ src/TablesList.java
@@ -30,13 +30,16 @@ public class TablesList
         return tablesList.get(i);
     }
     
-    public void assignTable(MembersList membersList) {
+    public void assignTable(MembersList membersList) throws IllegalArgumentException {
         int membersPerTable  = membersList.membersNumber() / this.tablesNumber();
+        if(membersPerTable == 0) {
+            throw new IllegalArgumentException("Number of Student is Less than Number of Tables");
+        }
         int remainder  = membersList.membersNumber() % tablesNumber();
         
         for(int i = 0, j = 0; i < membersList.membersNumber(); i++) {
             membersList.getMember(i).join(this.getTable(j));
-            if((i + 1) % membersPerTable == 0) {
+            if((i + 1) % membersPerTable == 0 && j < tablesList.size() - 1) {
                 j++;
             }
                 
--- src/miniWorld.java
+++ src/miniWorld.java
@@ -19,32 +19,56 @@ public class miniWorld extends World
     public miniWorld() {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1600, 900, 1);
-        prepare();
+        
+        String fileName = "/home/fcm2009/membersList";
+        try {
+            membersList = new MembersList(fileName);
+        } catch(FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Internal Erorr, Try Again Later", "Erorr", JOptionPane.ERROR_MESSAGE);
+        }
+        
+
+        int tablesNumber = 0;
+        int flag  = 0;
+        while(flag == 0) {
+            try {
+                String tmp = (JOptionPane.showInputDialog(null, "Enter Number of Groups Pleaes (Must Not Exceed Six)"));
+                if(tmp == null) {
+                    flag = -1;
+                    break;
+                }
+                else
+                    tablesNumber = Integer.parseInt(tmp);
+                if(tablesNumber <= 6 && tablesNumber > 0)
+                    flag = 1;
+                else
+                    throw new IllegalArgumentException();
+            } catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "Invalid Input, Try Again", "Erorr", JOptionPane.ERROR_MESSAGE);
+            } catch(IllegalArgumentException e){
+                JOptionPane.showMessageDialog(null, "Invalid Input, Try Again", "Erorr", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        
+        if(flag == 1)
+            prepare(tablesNumber);
     }
 
     /**
      * Prepare the world for the start of the program. That is: create the initial
      * objects and add them to the world.
      */
-    private void prepare() {
-        String fileName = "/home/fcm2009/membersList";
-        try {
-            membersList = new MembersList(fileName);
-        } catch(FileNotFoundException e) {
-            JOptionPane.showMessageDialog(null, "File Is Not Found or You Do Not Have Enough Privileges", "Erorr", JOptionPane.ERROR_MESSAGE);
-        }
+    private void prepare(int tablesNumber) {
         membersList.shuffle();
         
-        int tables = 6;
-        TablesList tablesList = new TablesList(tables);
-        
+        TablesList tablesList = new TablesList(tablesNumber);
+
+        tablesList.assignTable(membersList);
+
         this.addMembers(membersList);
         this.addTables(tablesList);
-        
-        tablesList.assignTable(membersList);
-        
     }
-    
+
     public void addTables(TablesList tablesList) {
         int hShift = 300;
         int vShift = 65;
@@ -53,12 +77,12 @@ public class miniWorld extends World
             this.addObject(tablesList.getTable(i - 1), hShift, vShift);
             hShift += 500;
             if(i % tablesPerLine == 0) {
-               hShift = 300;
-               vShift = 200;
+                hShift = 300;
+                vShift = 200;
             }
         }
     }
-    
+
     public void addMembers(MembersList membersList) {
         int hShift = 130;
         int vShift = 370;
